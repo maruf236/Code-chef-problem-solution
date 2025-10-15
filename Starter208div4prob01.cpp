9222bb3a178acb0da22558846fdd9911ab493b8a
Ali Ahsan Maruf <122002aliahsanmaruf@gmail.com>
2025-10-15 22:36:06 +0600

    Add files via upload
    Easy

---
+++ Starter208div4prob01.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	cout<<(x*30)+(y*60)<<endl;
+
+}
