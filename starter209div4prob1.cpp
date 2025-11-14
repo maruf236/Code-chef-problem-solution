381d9915da3ec8fa34a5d8f3cac2ee793b21ed51
Ali Ahsan Maruf <122002aliahsanmaruf@gmail.com>
2025-11-15 00:44:13 +0600

    Add files via upload
    easy

---
+++ starter209div4prob1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include<string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	// your code goes here
+int n;
+cin>>n;
+if(n<=4)
+{
+    cout<<"YES"<<endl;
+}
+else
+{
+    cout<<"NO"<<endl;
+}
+return 0;
+}
