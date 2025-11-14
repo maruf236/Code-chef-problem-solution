7a7b7c116d595687533bc1dc546261a828461ac2
Ali Ahsan Maruf <122002aliahsanmaruf@gmail.com>
2025-11-15 00:59:04 +0600

    207-*1

---
+++ Starter207div4prob1.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include<string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+
+   	int n;
+	cin>>n;
+	cout<<7-n<<endl;
+
+
+}
