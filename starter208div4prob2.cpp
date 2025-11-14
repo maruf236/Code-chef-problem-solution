6e6902d122a7d80c10b8c80f0bf85dddc6eaebd5
Ali Ahsan Maruf <122002aliahsanmaruf@gmail.com>
2025-11-15 00:54:40 +0600

    Add files via upload
    208-*2

---
+++ starter208div4prob2.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include<string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    int n,x,k,mr=0;
+	    cin>>n>>x>>k;
+	    vector<int>arr(n);
+	    for(int j=0;j<n;j++)
+	    {
+	        cin>>arr[j];
+	    }
+	    sort(arr.rbegin(),arr.rend());
+	    for(int j=0;j<k;j++)
+	    {
+	        arr[j]=0;
+	        x+=100;
+	    }
+	    arr.push_back(x);
+	    sort(arr.rbegin(),arr.rend());
+	    for(int j=0;j<=n;j++)
+	    {
+	        if(arr[j]==x)
+	        {
+	            cout<<j+1<<endl;
+	            break;
+	        }
+	    }
+
+
+	}
+	return 0;
+
+}
