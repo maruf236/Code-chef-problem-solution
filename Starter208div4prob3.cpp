fd3723b3cf4f0051cfc5df5ec2d2c9b7ff26ba87
Ali Ahsan Maruf <122002aliahsanmaruf@gmail.com>
2025-11-15 00:56:35 +0600

    Add files via upload
    208-*3

---
+++ Starter208div4prob3.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include<string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+
+    int n;
+cin>>n;
+for(int i=0;i<n;i++)
+{
+    int k,end=0;
+    cin>>k;
+    int a=0;
+    end=k;
+    while(a<k)
+    {
+        a++;
+        if(a%2==0)
+        {
+           end=end+(k-a);
+        }
+        else
+        {
+            end=end-(k-a);
+        }
+    }
+    cout<<end<<endl;
+}
+
+}
